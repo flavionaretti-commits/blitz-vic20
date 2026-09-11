b21070be58d08934f544ed91155c150742475826
Shankh <flavio.naretti@gmail.com>
2026-09-11 11:27:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'blitz-vic20-v3';
+const CACHE = 'blitz-vic20-v4';
 const LOCAL = [
   './', './index.html', './blitz.sb3', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'
